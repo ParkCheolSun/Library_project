84d9c59eaa7c9d26cde41991bf8fe49ae2b56641
ParkCheolSun <SeolSi@gmail.com>
2022-11-08 13:37:24 +0900

    [2022.11.08] 회원가입 HTML, CSS 추가

--- src/main/java/com/library/controller/MainController.java
+++ src/main/java/com/library/controller/MainController.java
@@ -1,9 +1,13 @@
 package com.library.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+//시큐리티 하기전이라 무시중
+@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class MainController {
 	@GetMapping(value = "/")
 	public String main() {

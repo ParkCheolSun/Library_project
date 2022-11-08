4b7bfde6bed5eb5d55c44800f497fd47f432190e
ParkCheolSun <SeolSi@gmail.com>
2022-11-08 10:13:56 +0900

    [2022.11.08] 회원가입 Entity, repository 생성

--- src/main/java/com/library/entity/Member.java
+++ src/main/java/com/library/entity/Member.java
@@ -1,16 +1,21 @@
 package com.library.entity;
 
+import java.net.InetAddress;
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import com.library.constant.Role;
 
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -23,14 +28,29 @@ import lombok.ToString;
 public class Member {
 	@Id
 	@Column(name = "id")
-	private Long id;
-	private String name;
+	@NotEmpty(message = "아이디 입력은 필수 입니다.")
+	private String id;					// 아이디
+	
+	@NotEmpty(message = "이름 입력은 필수 입니다.")
+	@NotNull
+	private String name;				// 이름
 	
 	@Column(unique = true)
-	private String email;
-	private String password;
-	private String address;
+	@Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일 형식에 맞지 않습니다.")
+	@NotNull
+	private String email;				// 이메일
+	
+	@NotEmpty(message = "비밀번호 입력은 필수 입니다.")
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+	@NotNull
+	private String password;			// 비밀번호
+	private String address;				// 주소
+	private String gender;				// 성별
 	
 	@Enumerated(EnumType.STRING)
-	private Role role;
+	private Role role;					// 권한
+	
+	private LocalDateTime lastLogin;	// 마지막 접속 날짜
+	
+	private InetAddress ipAddress;		// 접속한 ip주소
 }
---
+++ src/main/java/com/library/repository/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.library.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.library.entity.Member;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	Member findById(String id);
+}

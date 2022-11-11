3a92c8757b69d70015a1b9fa21afbaa525ec5d5e
Yun yeong Lee <dbsdud789@gmail.com>
2022-11-11 10:31:30 +0900

    헤더 메뉴바 클릭시 STS콘솔에 누른내용 출력 작업완료

--- src/main/java/com/library/controller/MenuController.java
+++ src/main/java/com/library/controller/MenuController.java
@@ -9,9 +9,108 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value="/header")
 public class MenuController {
 	
+	// 도서관 안내
 	@GetMapping(value = "/test1")
 	public String test1(Model model){
-	System.out.println("도서관 안내 누름");
+	System.out.println("도서관 안내");
+	return "redirect:/";
+	}
+	
+	@GetMapping(value = "/a1")
+	public String a1(Model model){
+	System.out.println("인사말");
+	return "redirect:/";
+	}
+	
+	@GetMapping(value = "/a2")
+	public String a2(Model model){
+	System.out.println("도서관 소개");
+	return "redirect:/";
+	}
+	
+	@GetMapping(value = "/a3")
+	public String a3(Model model){
+	System.out.println("찾아오시는 길");
+	return "redirect:/";
+	}
+	
+	
+	// 문화 프로그램
+	@GetMapping(value = "/test2")
+	public String test2(Model model){
+	System.out.println("문화 프로그램");
+	return "redirect:/";
+	}
+	
+	@GetMapping(value = "/b1")
+	public String b1(Model model){
+	System.out.println("프로그램 신청");
+	return "redirect:/";
+	}
+	
+	
+	// 알람마당
+	@GetMapping(value = "/test3")
+	public String test3(Model model){
+	System.out.println("알람마당");
+	return "redirect:/";
+	}
+	
+	@GetMapping(value = "/c1")
+	public String c1(Model model){
+	System.out.println("공지사항");
+	return "redirect:/";
+	}
+	
+	
+	// 열린마당
+	@GetMapping(value = "/test4")
+	public String test4(Model model){
+	System.out.println("열린마당");
+	return "redirect:/";
+	}
+	
+	@GetMapping(value = "/d1")
+	public String d1(Model model){
+	System.out.println("질문과 답변");
+	return "redirect:/";
+	}
+	
+	@GetMapping(value = "/d2")
+	public String d2(Model model){
+	System.out.println("자주하는 질문");
+	return "redirect:/";
+	}
+	
+	@GetMapping(value = "/d3")
+	public String d3(Model model){
+	System.out.println("건의사항");
+	return "redirect:/";
+	}
+	
+	@GetMapping(value = "/d4")
+	public String d4(Model model){
+	System.out.println("포토 갤러리");
+	return "redirect:/";
+	}
+	
+	
+	// 작은 도서관
+	@GetMapping(value = "/test5")
+	public String test5(Model model){
+	System.out.println("작은도서관");
+	return "redirect:/";
+	}
+	
+	@GetMapping(value = "/e1")
+	public String e1(Model model){
+	System.out.println("도서관별 소개");
+	return "redirect:/";
+	}
+	
+	@GetMapping(value = "/e2")
+	public String e2(Model model){
+	System.out.println("도서관 소식");
 	return "redirect:/";
 	}
 }

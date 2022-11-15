f55514943bcd8aae46ad990e836a9265f3910bc9
ParkCheolSun <SeolSi@gmail.com>
2022-11-15 16:42:40 +0900

    [2022.11.15] Security 로그인 및 이메일 인증 적용

--- src/main/java/com/library/config/SecurityConfig.java
+++ src/main/java/com/library/config/SecurityConfig.java
@@ -32,14 +32,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**", "/lib/**", "/", "/login/**");  //로그인 여부를 조사 안할 요청값 입니다.
+        web.ignoring().antMatchers("/css/**", "/javascript/**", "/images/**", "/lib/**","/");  //로그인 여부를 조사 안할 요청값 입니다.
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
     	
         http.authorizeRequests()  //사용자의 로그인이 필요한 요청목록 입니다.
-        		.mvcMatchers("/css/**", "/javascript/**", "/images/**", "/lib/**", "/", "/member/**","/header/**","/login/**").permitAll()
+        		.mvcMatchers("/css/**", "/javascript/**", "/images/**", "/lib/**", "/","/login/**","/email/**").permitAll()
                 .mvcMatchers("/admin/**").hasRole("ADMIN") // /admin 경로 접근자는 ADMIN Role일 경우만 접근가능하도록 설정
                 .anyRequest().authenticated(); // 나머지 경로들은 모두 인증을 요구하도록 설정
                 

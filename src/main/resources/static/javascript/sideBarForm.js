903cf5f47f62cc8712135c901d6f0b07030be08c
ParkCheolSun <SeolSi@gmail.com>
2022-11-17 17:34:26 +0900

    [2022.11.17] SideBar js 수정

--- src/main/resources/static/javascript/sideBarForm.js
+++ src/main/resources/static/javascript/sideBarForm.js
@@ -1,24 +1,31 @@
 			$(document).ready(function() {
-					var pathName = $(location).attr('pathname');
-					if(pathName != "/"){
-						$('.components').find('a').each(function(index, item){
-							if($(item).attr('href') == "#homeSubmenu" || 
-									$(item).attr('href') == "#pageSubmenu" ||
-									$(item).attr('href') == "#likeSubmenu" ||
-									$(item).attr('href') == "#cultureMenu" ||
-									$(item).attr('href') == "#noticeMenu"){
-								var id = $(item).attr('id');
-								//console.log("id : " + id);
-								if(id == "guideMenu" || id == "cultureMenu" || id == "noticeMenu" ||
-										id == "openNoticeMenu" || id == "sLibraryMenu"){
-									var temp = "#" + id;
-									//console.log("완성문자 : " + temp);
-									$(temp).get(0).click();
-									return;
-								} else {
-									//console.log("test : " + $(item).parent().parent().parent().children('a').attr('id'));
-								}
+				var pathName = $(location).attr('pathname');
+				if(pathName != "/"){
+					$('.components').find('a').each(function(index, item){
+						if($(item).attr('href') == "#homeSubmenu" || 
+								$(item).attr('href') == "#pageSubmenu" ||
+								$(item).attr('href') == "#likeSubmenu" ||
+								$(item).attr('href') == "#cultureMenu" ||
+								$(item).attr('href') == "#noticeMenu"){
+							var id = $(item).attr('id');
+							//console.log("id : " + id);
+							if(id == "guideMenu" || id == "cultureMenu" || id == "noticeMenu" ||
+									id == "openNoticeMenu" || id == "sLibraryMenu"){
+								var temp = "#" + id;
+								//console.log("완성문자 : " + temp);
+								var search = $(item).parent().children('ul').children('li').children('a');
+								var len = search.length;
+								
+								search.each(function(idx, data){
+									if($(data).attr('href') == pathName){
+										$(temp).get(0).click();
+									}
+								});
+								return;
+							} else {
+								//console.log("test : " + $(item).parent().parent().parent().children('a').attr('id'));
 							}
-						});
-					}
-				});
+						}
+					});
+				}
+			});

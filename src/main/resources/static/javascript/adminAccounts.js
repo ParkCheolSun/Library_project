b601cf2249e9718a7796afef839637a91e12bbd6
ParkCheolSun <86757544+ParkCheolSun@users.noreply.github.com>
2022-12-03 15:46:35 +0900

    [2022.12.03] 관리자 페이지 회원 상세정보 모달로 출력

--- src/main/resources/static/javascript/adminAccounts.js
+++ src/main/resources/static/javascript/adminAccounts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-	$('#data_list').DataTable({
+	var table = $('#data_list').DataTable({
 		"language": {
             "decimal":        "",
             "emptyTable":     "표에서 사용할 수있는 데이터가 없습니다.",
@@ -25,6 +25,45 @@ $(document).ready(function() {
             }
         }
 	});
+	
+	table.column(4).visible(false);
+	table.column(5).visible(false);
+	table.column(6).visible(false);
+	
+	$("#data_list").on('click', 'tbody tr', function () {
+    	var row = $("#data_list").DataTable().row($(this)).data();
+    	console.log(row);
+    	$('#exampleModal').modal('show');
+    	$('#modal-detail-id').val(row[0]);
+    	$('#modal-detail-name').val(row[1]);
+    	$('#modal-detail-email').val(row[2]);
+    	$('#modal-detail-role').val(row[3]);
+    	$('#modal-detail-address').val(row[4]);
+    	$('#modal-detail-address_detail').val(row[5]);
+    	$('#modal-detail-gender').val(row[6]);
+    	$('#modal-detail-createDate').val(row[7]);
+	});
+	
+	async function detailMember(row){
+		var id = row[0];
+		const { value: formValues } = await Swal.fire({
+  			title: 'Multiple inputs',
+  			html:
+  				'<label>아이디 : </label>' +
+    			'<input id="swal-input1" class="swal2-input" value=id>' +
+    			'<input id="swal-input2" class="swal2-input">',
+  			focusConfirm: false,
+  			preConfirm: () => {
+    			return [
+      				row
+    			]
+  			}
+		})
+		if (formValues) {
+  			Swal.fire(JSON.stringify(formValues))
+		}
+	}
+  	
 	
 	$(document).on('click', 'button[name="detailBtn"]', function() {
      	var outlayId = $("#data_list").DataTable().row( $(this).parents('tr') ).data();
